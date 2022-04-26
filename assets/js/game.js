23d57abe102360d00077bb72648ab6fb5f5bbbbf
Marie Baptiste <mariebaptiste@Maries-MacBook-Air.local>
2022-04-26 02:53:48 -0400

    added fight function and window prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! JavaScript is running!")
+//FIGHT FUNCTION
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+window.prompt("what is your robot's name?");
+
+fight();
+
