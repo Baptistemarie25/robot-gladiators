dd7a67abc0ba5376d64b9bb975e17540be2aca3e
Baptistemarie25 <mariebaptiste@Maries-MacBook-Air.local>
2022-04-30 22:13:22 -0400

    initial gamesetup redo

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 var enemyName = "Roborto";
 var enemyHealth = 50;
@@ -43,7 +44,19 @@ var fight = function () {
     }
     //if player choose to skip 
 }   else if (promptFight === "skip" || promptFight === "SKIP") {
-    window.alert(playerName + " has chosen to skip the fight!");
+    // confirm player wants to skip 
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight 
+    if (confirmSkip) {
+        window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        //subtract money from the playermoney for skipping 
+        playerMoney = playerMoney - 2; 
+    }
+    //if no (false), ask the question again by running fight() again 
+    else {
+        fight();
+    }
 }   else {
     window.alert("You need to choose a valid option. Try again!");
 }};
